434c394a006bd6630d2a86c11e357f2ed970cc6e
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-05-16 11:43:10 +0100

    Delete unnecessay System.out.println.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,40 +15,25 @@ public class App {
       ArrayList<Integer> listOfCoins = new ArrayList<>();
       while  (number > 0) {
           if (number >= 50 ) {
-              System.out.println("number - 50");
               listOfCoins.add(50);
               number -= 50;
-              System.out.println(listOfCoins);
-              System.out.println(number);
           }
           while (number >= 20) {
-              System.out.println("number - 20");
               listOfCoins.add(20);
               number -= 20;
-              System.out.println(listOfCoins);
-              System.out.println(number);
           }
           while (number >= 10) {
-              System.out.println("number - 10");
               listOfCoins.add(10);
               number -= 10;
-              System.out.println(listOfCoins);
-              System.out.println(number);
           }
 
           while (number >= 5) {
-              System.out.println("number - 5");
               listOfCoins.add(5);
               number -= 5;
-              System.out.println(listOfCoins);
-              System.out.println(number);
           }
           while (number >= 2) {
-              System.out.println("number - 2");
               listOfCoins.add(2);
               number -= 2;
-              System.out.println(listOfCoins);
-              System.out.println(number);
           }
       }
       return listOfCoins.size();
