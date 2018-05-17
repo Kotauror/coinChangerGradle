1b486a7de10eb94190dc40383136cf4f441d4572
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-05-17 10:29:15 +0100

    Further renaming.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,20 +10,20 @@ public class App {
       Collections.addAll(listOfCoins, 50, 20, 10, 5, 2, 1);
     }
 
-    public int coinChanger(int number) {
+    public int coinChanger(int numberToChange) {
         if (listOfCoins.size() > 0) {
-          decreaseByCoin(number, listOfCoins.get(0));
+          decreaseByACoin(numberToChange, listOfCoins.get(0));
         }
         return CoinsOfUser.size();
     }
 
-    public int decreaseByCoin(int number, int coin) {
-        while(number >= coin) {
+    public int decreaseByACoin(int numberToChange, int coin) {
+        while(numberToChange >= coin) {
             CoinsOfUser.add(coin);
-            number -= coin;
+            numberToChange -= coin;
         };
         listOfCoins.remove(0);
-        coinChanger(number);
-        return number;
+        coinChanger(numberToChange);
+        return numberToChange;
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -15,16 +15,16 @@ public class AppTest {
     }
 
     @Test public void decreaseByCoin_50() {
-      assertEquals("case for 50", 0, tester.decreaseByCoin(50, 50));
+      assertEquals("case for 50", 0, tester.decreaseByACoin(50, 50));
     }
     @Test public void decreaseByCoin_40() {
-      assertEquals("case for 40", 0, tester.decreaseByCoin(40, 20));
+      assertEquals("case for 40", 0, tester.decreaseByACoin(40, 20));
     }
     @Test public void decreaseByCoin_30() {
-      assertEquals("case for 30", 10, tester.decreaseByCoin(30, 20));
+      assertEquals("case for 30", 10, tester.decreaseByACoin(30, 20));
     }
     @Test public void decreaseByCoin_14() {
-      assertEquals("case for 14", 4, tester.decreaseByCoin(14, 5));
+      assertEquals("case for 14", 4, tester.decreaseByACoin(14, 5));
     }
     @Test public void returnsNumberOfCoins50() {
         assertEquals("case for 50", 1, tester.coinChanger(50));
