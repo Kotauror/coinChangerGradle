246a503591f868b0eca265e6e67e1b9adce69e7c
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-05-21 12:10:48 +0100

    Moved @BeforeEach and @Test to separate lines.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -13,28 +13,37 @@ public class AppTest {
 
     private static Logger log = Logger.getLogger("jusia");
 
-    @BeforeEach public void initObject() {
+    @BeforeEach 
+    public void initObject() {
         app = new App();
     }
-    @Test public void returnsArrayOfCoins50() {
+
+    @Test
+    public void returnsArrayOfCoins50() {
         ArrayList<Integer> afterOperation = app.changeCoins(50, 0);
         ArrayList<Integer> expectedOutput = new ArrayList<>();
         Collections.addAll(expectedOutput, 50);
         assertTrue(afterOperation.equals(expectedOutput));
     }
-    @Test public void returnsArrayOfCoins20() {
+
+    @Test
+    public void returnsArrayOfCoins20() {
         ArrayList<Integer> aftOpTwo = app.changeCoins(20, 0);
         ArrayList<Integer> expectedOutputTwo = new ArrayList<>();
         Collections.addAll(expectedOutputTwo, 20);
         assertTrue(aftOpTwo.equals(expectedOutputTwo));
     }
-    @Test public void returnsArrayOfCoins73() {
+
+    @Test
+    public void returnsArrayOfCoins73() {
         ArrayList<Integer> aftOpTwo = app.changeCoins(73, 0);
         ArrayList<Integer> expectedOutputTwo = new ArrayList<>();
         Collections.addAll(expectedOutputTwo, 50, 20, 3, 1);
         assertTrue(aftOpTwo.equals(expectedOutputTwo));
     }
-    @Test public void returnsArrayOfCoins53() {
+
+    @Test 
+    public void returnsArrayOfCoins53() {
         ArrayList<Integer> aftOpTwo = app.changeCoins(53, 0);
         ArrayList<Integer> expectedOutputTwo = new ArrayList<>();
         Collections.addAll(expectedOutputTwo, 50, 2, 1);
